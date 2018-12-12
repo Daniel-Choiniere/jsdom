fd9d2d42fab73feac69ef97d81c59cdccd4522a7
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-12 05:05:55 +0000

    quersy selector using jQuery and vanilla js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const wrap = document.querySelector('#book-list li:nth-child(2) .name');;
-console.log(wrap);
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+console.log(wmf);
 
+// will return only one element (the first one in the document)
+var books = document.querySelector('#book-list li .name');
+console.log(books);
+
+// will return every element with the 
+books = document.querySelectorAll('#book-list li .name');
+console.log(books);
+
+Array.from(books).forEach(function(book){
+  console.log(book);
+});
