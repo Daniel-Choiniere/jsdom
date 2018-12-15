c54bf955c2aca237f7cfa4a9f4f44dcd3c784cea
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-15 13:14:13 +0000

    used eventlisteners with keyup to filter user search query

--- app.js
+++ app.js
@@ -44,3 +44,17 @@ hideBox.addEventListener('change', function(e){
   }
 });
 
+// filter books when user types into sesrch bar
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e) {
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName('li');
+  Array.from(books).forEach(function(book) {
+    const title = book.firstElementChild.textContent;
+    if(title.toLowerCase().indexOf(term) > 0){
+      book.style.display = 'block';
+    } else {
+      book.style.display = 'none';
+    }
+  });
+});
