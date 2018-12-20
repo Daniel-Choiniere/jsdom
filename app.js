59b6919643b3d3898ee4196faf096f1d636a9f1d
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-20 13:07:31 +0000

    working on fixing bug with tab content not switching

--- app.js
+++ app.js
@@ -63,20 +63,19 @@ hideBox.addEventListener('change', function(e){
   });
 
 // tabbed content
-const tabs = document.querySelector('.tabs');
-const panels = document.querySelectorAll('.panel');
-tabs.addEventListener('click', function(e) {
-  if(e.target.tagName == 'LI'){
-    // console.log("test");
-    const targetPanel = document.querySelector(e.target.dataset.target);
-    panels.forEach(function(panel) {
-      if(panel == targetPanel){
-        panel.classList.add('active');
-      }else{
-        panel.classList.remove('active');
-      }
-    })
-  }
-});
-  
+  const tabs = document.querySelector('.tabs');
+  const panels = document.querySelectorAll('.panel');
+  tabs.addEventListener('click', (e) => {
+    if(e.target.tagName == 'LI'){
+      const targetPanel = document.querySelector(e.target.dataset.target);
+      Array.from(panels).forEach((panel) => {
+        if(panel == targetPanel){
+          panel.classList.add('active');
+        }else{
+          panel.classList.remove('active');
+        }
+      });
+    }
+  });
+
 })
